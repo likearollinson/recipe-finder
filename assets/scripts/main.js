ab815ee29dc1dc2a55eb4e30e6b60f6e99885fae
Tucker Barrett <tuckerbarrett@gmail.com>
2021-07-21 09:02:24 -0400

    Adds elements with recipe data, not yet shown to page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -87,12 +87,63 @@ function showRecipes(recipes) {
         }
 
         allFoodRecipes.push(nextRecipe);
+
+        // Create elements
+
+        // Section element created just for testing
+        let nextSection = document.createElement("section");
+
+        let nextCard = document.createElement("article");
+        nextCard.setAttribute("class", "card");
+
+        let nextImageDiv = document.createElement("div");
+        nextImageDiv.setAttribute("class", "card-image");
+
+        let nextFigure = document.createElement("figure");
+        nextFigure.setAttribute("class", "image is-4by3");
+        
+        let nextImage = document.createElement("img");
+        nextImage.setAttribute("src", nextRecipe.image);
+
+        let nextCardContentDiv = document.createElement("div");
+        nextCardContentDiv.setAttribute("class", "card-content");
+
+        let nextMediaDiv = document.createElement("div");
+        nextMediaDiv.setAttribute("class", "media");
+
+        let nextMediaContent = document.createElement("div");
+        nextMediaContent.setAttribute("class", "media-content");
+
+        let nextRecipeName = document.createElement("p");
+        nextRecipeName.setAttribute("class", "title is-4 has-text-black");
+        nextRecipeName.textContent = nextRecipe.name;
+
+        let nextRecipeURL = document.createElement("p");
+        nextRecipeURL.setAttribute("class", "subtitle is-6 has-text-black");
+        let nextLink = document.createElement("a");
+        nextLink.setAttribute("href", nextRecipe.url);
+
+        // Append
+        nextSection.appendChild(nextCard);
+        nextCard.appendChild(nextImageDiv);
+        nextCard.appendChild(nextCardContentDiv);
+        nextImageDiv.appendChild(nextFigure);
+        nextFigure.appendChild(nextImage);
+        
+        nextCardContentDiv.appendChild(nextMediaDiv);
+        nextMediaDiv.appendChild(nextMediaContent);
+        nextMediaContent.appendChild(nextRecipeName);
+        nextMediaContent.appendChild(nextRecipeURL);
+
+        nextSection.style.display = "block";
+
+
     }
     console.log(allFoodRecipes);
 
     // TODO:
     
-    // Show modal
+    // Define and show modal
 
     // Loop through each recipe, creating card elements for each one, adding the necessary info to each card
 }
